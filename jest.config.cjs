fb56310d7f125c0ff6949af12bf5fd83301f9710
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-31 03:37:51 -0300

    chore: ajustando configuração de testes e build

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+  '^.+\\.(ts|tsx)$': ['ts-jest', {
+    tsconfig: 'tsconfig.test.json',
+  }],
+},
+};
