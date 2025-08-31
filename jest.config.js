3acafce8306afbad56c54a05a7fe935ca68949ef
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-30 23:55:03 -0300

    chore: adiciona configuração de ESLint e Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
