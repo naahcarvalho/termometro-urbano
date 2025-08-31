1bd1028bc6d9e05c145be54b64aa8896e8f34244
Naah Carvalho <naah_.carvalho@outlook.com>
2025-08-31 03:38:27 -0300

    chore: removendo arquivos não utilizados

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
